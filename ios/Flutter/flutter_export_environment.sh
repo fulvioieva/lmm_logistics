63df54e69176b37be9491d9ab543841e7aecaa86
Giada Barbero <giada.barbero@iscstorino.com>
2019-09-13 16:42:35 +0200

    update version of build ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\andre\Desktop\lmm_logistics"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/syncroweb/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/syncroweb/lmm_logistics"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\SDK\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/syncroweb/Desktop/flutter/bin/cache/artifacts/engine/ios-release"
